703adac8629e413aaa742b28e6209f39c07cc799
yunjeong <yunjeong7222@gmail.com>
2026-03-13 16:58:43 +0900

    feat : db 로직 수정

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,15 +2,19 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const conn = mysql.createConnection({
+const conn= mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  // 추가 설정 (서버 환경에 맞게 조절 가능)
+  connectionLimit: 10,        // 동시에 유지할 연결 수
+  waitForConnections: true,   // 연결이 꽉 찼을 때 대기 여부
+  queueLimit: 0               // 대기 줄의 제한 (0은 무제한)
 });
 
-conn.connect((err) => {
+conn.getConnection((err) => {
   if (err) {
     console.error("MySQL 연결 실패:", err.message);
     return;
