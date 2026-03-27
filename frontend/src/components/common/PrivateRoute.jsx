846895be7a2cf89023402bcc80504c37b3851634
blaxxswan <blaxxswan@naver.com>
2026-03-27 17:48:59 +0900

    feat: sweetalert2 수정-수완

--- frontend/src/components/common/PrivateRoute.jsx
+++ frontend/src/components/common/PrivateRoute.jsx
@@ -1,14 +1,14 @@
-import { Navigate } from 'react-router-dom';
+import {Navigate} from 'react-router-dom';
 import useAuthStore from '../../store/authStore';
+// import Alert from '../../utils/alert';
 
-const PrivateRoute = ({ children }) => {
-  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
-  if (!isLoggedIn) {
-    alert('로그인이 필요한 서비스입니다.');
-    return <Navigate to="/login" replace />;
-  }
+const PrivateRoute = ({children}) => {
+    const user = useAuthStore((state) => state.user);
+    if (!user) {
+        return <Navigate to="/" replace />;
+    }
 
-  return children;
+    return children;
 };
 
 export default PrivateRoute;
--- frontend/src/utils/alert.js
+++ frontend/src/utils/alert.js
@@ -1,19 +1,19 @@
 import Swal from 'sweetalert2';
 
 const Alert = Swal.mixin({
-  background : '#f8fafc no-repeat center',
-  confirmButtonColor: '#9c88ff',
-  cancelButtonColor: '#aaa',
-  width: '320px',
-  padding : '20px',
-  customClass: {
-    popup: 'swal-custom-popup',
-    icon: 'swal-custom-icon',
-    title: 'swal-custom-title',
-    text : 'swal-custom-text',
-    confirmButton: 'swal-custom-confirm',
-    cancelButton: 'swal-custom-cancel',
-  },
+    background: '#f8fafc',
+    confirmButtonColor: '#9c88ff',
+    cancelButtonColor: '#aaa',
+    width: '350px',
+    padding: '40px 20px',
+    customClass: {
+        popup: 'swal-custom-popup',
+        icon: 'swal-custom-icon',
+        title: 'swal-custom-title',
+        text: 'swal-custom-text',
+        confirmButton: 'swal-custom-confirm',
+        cancelButton: 'swal-custom-cancel',
+    },
 });
 
 export default Alert;
